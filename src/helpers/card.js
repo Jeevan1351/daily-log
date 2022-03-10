60215c432795e9ffe89b21ea4ab0d36c3eacc1f8
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-03-10 09:50:18 +0530

    Adding design elements

--- src/helpers/card.js
+++ src/helpers/card.js
@@ -1,12 +1,18 @@
 function Card(value) {
-    
+    console.log(value)
     return (value)?<><div className='card' style={{color: `red`}}>
-            <p>{value.value.day} {value.value.date}</p>
-            <ul>
-                {value.value.activities && value.value.activities.map((act, index) => {
-                    return <li key={index}><p>{act}</p></li>
-                })}
-            </ul>
+            <p id='title'>{value.value.day} {value.value.date}</p>
+            <div id='activities'>
+                <ul>
+                    {value.value.activities && value.value.activities.map((act, index) => {
+                        return <li key={index}><p>{act}</p></li>
+                    })}
+                </ul>
+            </div>
+            <div className="add">
+                <input type='text' placeholder="Washed the cat"></input>
+                <button>+</button>
+            </div>  
         </div>
     </>:<></>
 }
